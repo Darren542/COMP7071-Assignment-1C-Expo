d5795ad0209e380f73794b7976fe696565e488a8
Darren542 <Darrenluck@icloud.com>
2025-02-09 18:56:20 -0800

    API Location Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,36 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [ message, setMessage ] = useState('Loading...');
+  const API_URL = Platform.select({
+    ios: 'https://assignment1cbackend-fkcygyanhxejd8cm.canadacentral-01.azurewebsites.net/test',
+    android: 'http://10.0.2.2:5138/test',
+    web: 'http://localhost:5138/test',
+    default: 'http://localhost:5138/test',
+  });
+
+  const fetchMessage = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      // console.error(response.data);
+      setMessage(response.data);
+    } catch (error) {
+      console.error(error);
+      setMessage('Error fetching data');
+    }
+  };
+
+  // useEffect(() => {
+  //   fetchMessage();
+  // }, []);
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -20,35 +45,14 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">This is a Demo of using React Native</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+          It involves a ASP.Net Core Web API for the backend and a React Native app for the frontend.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <ThemedText type="subtitle">Message from the backend: {message}</ThemedText>
+        <Button title="Fetch Message" onPress={fetchMessage} />
       </ThemedView>
     </ParallaxScrollView>
   );
